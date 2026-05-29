684dd4bd0e9097d569de36eaf47b360d6431f8d7
Sander <vreijsander@gmail.com>
2026-05-29 11:33:40 +0200

    1e lvl layout af! behalve boss arena

---
+++ Assets/Enemies/Hazards/Hazard_onTouch.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Hazard_onTouch : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<IDamageable>()?.TakeDamage(999999);
+            
+        }
+    }
+}
