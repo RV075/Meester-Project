3910088b56e149674394a8f04bec7f462dd473ec
Sander <vreijsander@gmail.com>
2026-06-24 13:38:33 +0200

    added orb texture

---
+++ Assets/Other/Scripts/Dino_walk.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MoveRightPhysics : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+}
